194ee9e58c2bd82f3e4d51cf9cf12ac775f596a0
Andrew Trotman <andrew@trotman.com>
2021-03-27 21:16:45 +1300

    FLEX compatible serial routines

--- serial.asm
+++ serial.asm
@@ -9,8 +9,10 @@ serial_control EQU $E004
 serial_data		EQU $E005
 
 ;
+; bit 0 is the receive data register full bit (1 = data ready to be read, 0 = no data)
 ; bit 1 is the transmit data register empty bit (1 = clear to send, 0 = busy)
 ;
+serial_status_rdrf		EQU %00000001
 serial_status_tdre		EQU %00000010
 
 ;
@@ -29,6 +31,7 @@ serial_reset EQU %00000011
 ;	SERIAL_INIT
 ;	-----------
 ;
+FLEX_TINIT		; TERMINAL INITIALIZATION
 serial_init
 	LDA #serial_reset
 	STA serial_control
@@ -37,13 +40,56 @@ serial_init
 	RTS
 
 ;
+;	SERIAL_PEEK
+;	-----------
+;	Check to see if a character is ready to be read, and if so then set
+FLEX_STAT		; CHECK TERMINAL STATUS
+	PSHS A
+	LDA serial_status
+	BITA #serial_status_rdrf
+	PULS A
+	RTS
+
+;
+;	SERIAL_GETCHAR
+;	--------------
+;	Blocking read of a character from the serial port, and return in A
+;
+FLEX_INCHNE		; INPUT CHARACTER W/O ECHO
+serial_getchar
+	LDA serial_status					; for a character to arrive
+	BITA #serial_status_rdrf
+	BEQ serial_getchar
+
+	LDA serial_data					; read the character that has arrived
+	RTS
+
+;
+;	SERIAL_GETCHAR_ECHO
+;	-------------------
+;	Blocking read of a character from the serial port, echo, and return in A
+;
+FLEX_INCH		; INPUT CHARACTER W/ ECHO
+serial_getchar_echo
+	BSR serial_getchar
+	BSR serial_putchar
+	RTS
+
+;
 ;	SERIAL_PUTCHAR
 ;	--------------
 ;	Print the character in A
 ;
+FLEX_OUTCH		; OUTPUT CHARACTER
 serial_putchar
-	STA serial_data
-	BSR serial_wait
+	PSHS A								; wait for clear to write
+serial_putchar_wait
+	LDA serial_status
+	BITA #serial_status_tdre
+	BEQ serial_putchar_wait
+	PULS A
+
+	STA serial_data					; write
 	RTS
 
 ;
@@ -60,18 +106,4 @@ serial_putchar_next
 	BRA serial_putchar_next
 serial_puts_done
 	PULS A,X
-	RTS
-
-;
-;	SERIAL_WAIT
-;	-----------
-;	Wait until status register indicates ready
-;
-serial_wait
-	PSHS A
-serial_wait_longer
-	LDA serial_status
-	BITA #serial_status_tdre
-	BEQ serial_wait_longer
-	PULS A
 	RTS
